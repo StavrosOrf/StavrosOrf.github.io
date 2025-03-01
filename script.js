2e99616cb57fa71a161da2d09a7a7386b867791e
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2025-03-01 01:41:29 +0100

    updating

--- script.js
+++ script.js
@@ -21,12 +21,51 @@ function loadTab(tabName) {
   
       // Load the corresponding tab content based on the data-tab attribute
       let tab = this.getAttribute('data-tab');
-      loadTab(tab);
+      if (tab !== null)
+        loadTab(tab);
     });
   });
-  
-  // Load the default tab (home) on page load
-  window.addEventListener('DOMContentLoaded', () => {
+
+// Load user preference on page load
+window.addEventListener('DOMContentLoaded', () => {
     loadTab('home');
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+      document.body.classList.add('dark-theme');
+      updateToggleIcon(true);
+    }
   });
+  
+  // Grab the toggle button
+  const themeToggleBtn = document.getElementById('theme-toggle');
+  themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    
+    // Check if dark theme is active
+    const isDark = document.body.classList.contains('dark-theme');
+    
+    // Store user preference
+    if (isDark) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+    
+    // Update the icon
+    updateToggleIcon(isDark);
+  });
+  
+  // Update icon function
+  function updateToggleIcon(isDark) {
+    const icon = themeToggleBtn.querySelector('i');
+    if (isDark) {
+      // Switch to sun icon
+      icon.classList.remove('fa-moon');
+      icon.classList.add('fa-sun');
+    } else {
+      // Switch to moon icon
+      icon.classList.remove('fa-sun');
+      icon.classList.add('fa-moon');
+    }
+  }
   
